cb1d9e20c5f6d657c4c087216d59712c45dbc4ca
edsonlb <ti_edson@yahoo.com.br>
2019-04-27 13:29:59 +0200

    3 Adding GraphQL

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req,res, next) => {
-    res.send('Hello Word');
-});
+app.use('/graphql', graphqlHttp({
+    graphiql: true,
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Tomantic', 'test', 'asdasdasd'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName + " test...";
+
+        }
+    }
+}));
 
 app.listen(3000);
